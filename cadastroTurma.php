7e8665a633042f56864943c882afd61f89de7325
GuiFFelipe <guilhermefelipe013@gmail.com>
2021-10-17 17:26:11 -0300

    .

---
+++ cadastroTurma.php
@@ -0,0 +1,22 @@
+<?php
+//Abre conexao com banco
+include 'conexao.php';
+
+//recebimento dos dados das váriaveis
+$codigo = $_POST['codigo'];
+$descricao = $_POST['descricao'];
+$situacao = $_POST['situacao'];
+
+//$recebendo_cadastros é onde puxo todos os dados
+$recebendo_cadastros = "INSERT INTO 
+curso
+VALUES ('$codigo',
+        '$descricao',
+        '$situacao')";
+
+        //query_cadastros = recebe como parametros
+        //conexao do banco e dados do cadastros
+        $query_cadastros = mysqli_query($connx, $recebendo_cadastros);
+
+        //Faz voltar a página cad_curso.php
+        header('location:cad_curso.html');
